2c8612cc7898074dd114ae3cfc3b12b1f032eab6
Touhidul islam <touhidulislam066@gmail.com>
2025-12-23 22:24:37 +0300

    Solve PART 3: Practical Coding

---
+++ Calculator.java
@@ -0,0 +1,31 @@
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed");
+        }
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+
+        System.out.println("Addition: " + calc.add(10, 5));
+        System.out.println("Subtraction: " + calc.subtract(10, 5));
+        System.out.println("Multiplication: " + calc.multiply(10, 5));
+        System.out.println("Division: " + calc.divide(10, 5));
+    }
+}
